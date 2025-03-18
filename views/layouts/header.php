b3c58af60aafb569749c683e5f32055ebe6e997c
Chhorrina-THIN <chhorrina.thin@student.passerellesnumeriques.org>
2025-03-18 14:58:32 +0700

    feat(Dashboard)Dashboard UI Design and Layout[CV1-47]

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -17,18 +17,19 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <!-- CSS Files -->
   <link id="pagestyle" href="views/assets/css/argon-dashboard.css?v=2.1.0" rel="stylesheet" />
 </head>
 
 <body class="g-sidenav-show   bg-gray-100">
-  <div class="min-height-300 bg-dark position-absolute w-100"></div>
-  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
+  <div class="min-height-300 bg-dark position-fixed w-100"></div>
+  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
         <img src="views/assets/img/small-logos/logo.png" width="26px" height="26px" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Lim Try</span>
+        <span class="ms-1 font-weight-bold fs-6">Lim Try</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
@@ -39,7 +40,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Dashboard</span>
+        <span class="nav-link-text ms-1 fs-6">Dashboard</span>
         </a>
       </li>
       <li class="nav-item">
@@ -47,7 +48,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-calendar-grid-58 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Inventory</span>
+        <span class="nav-link-text ms-1 fs-6">Inventory</span>
         </a>
       </li>
       <li class="nav-item">
@@ -55,7 +56,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Sales</span>
+        <span class="nav-link-text ms-1 fs-6">Sales</span>
         </a>
       </li>
       <li class="nav-item">
@@ -63,7 +64,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-app text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Customer</span>
+        <span class="nav-link-text ms-1 fs-6">Customer</span>
         </a>
       </li>
       <li class="nav-item">
@@ -71,7 +72,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-world-2 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Supplier</span>
+        <span class="nav-link-text ms-1 fs-6">Supplier</span>
         </a>
       </li>
       <li class="nav-item">
@@ -79,7 +80,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-world-2 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Employee</span>
+        <span class="nav-link-text ms-1 fs-6">Employee</span>
         </a>
       </li>
       <li class="nav-item">
@@ -87,7 +88,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Account</span>
+        <span class="nav-link-text ms-1 fs-6">Account</span>
         </a>
       </li>
       <li class="nav-item">
@@ -95,7 +96,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-single-copy-04 text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Report</span>
+        <span class="nav-link-text ms-1 fs-6">Report</span>
         </a>
       </li>
       <li class="nav-item">
@@ -103,7 +104,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-collection text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Help and Support</span>
+        <span class="nav-link-text ms-1 fs-6">Help and Support</span>
         </a>
       </li>
       <li class="nav-item">
@@ -111,24 +112,15 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-collection text-dark text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Settings</span>
+        <span class="nav-link-text ms-1 fs-6">Settings</span>
         </a>
       </li>
       </ul>
         </div>
-        <style>
-      .nav-link:hover, .nav-link-text:hover {
-      background-color: #f8f9fa;
-      color: #5e72e4;
-      }
-      .nav-link.active, .nav-link-text.active {
-      background-color: #5e72e4;
-      color: #fff;
-      }
-        </style>
+
     <div class="sidenav-footer mx-3 ">
       <div class="card card-plain shadow-none" id="sidenavCard">
-        <img class="w-50 mx-auto" src="../assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
+        <img class="w-50 mx-auto" src="/views/assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
         <div class="card-body text-center p-3 w-100 pt-0">
           <div class="docs-info">
             <h6 class="mb-0">Need help?</h6>
