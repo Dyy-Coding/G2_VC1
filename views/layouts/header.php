d579765aac221a962da3315078e2a8826a95ca66
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-03-24 08:02:59 +0700

    fix sidecar style

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -34,7 +34,7 @@
         aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" #"
         target="_blank">
-        <img src="views/assets/img/logos/logo.png" width="26px" height="26px" class="navbar-brand-img h-100"
+        <img src="views/assets/img/logo-ct-dark.png" width="30%" height="30%" class="navbar-brand-img h-100"
           alt="main_logo">
         <span class="ms-1 font-weight-bold">Lim Try</span>
       </a>
