930d88379213f416251f50d4e94d398ed9a2abd9
Dyy-Coding <chandyneat9999@gmail.com>
2025-03-29 12:11:29 +0700

    feat(dashboard)supplier[CV1-318]

--- Controllers/adminController/DashboardController.php
+++ Controllers/adminController/DashboardController.php
@@ -19,12 +19,9 @@ public function index() {
     // Calculate the percentage change in customers
     $customerPercentageChange = $this->todayMoneyModel->getCustomerPercentageChange();
 
-<<<<<<< HEAD
-=======
     $totalSuppliers = $this->todayMoneyModel->getTotalSuppliers();
     $totalPurchaseorders = $this->todayMoneyModel->getTotalPurchaseorders();
     $percentageChange = -2; // xample: you can dynamically calculate this if needed
->>>>>>> main
     // Retrieve today's money data
     $todayMoneyData = $this->todayMoneyModel->getTodayMoneyData();
 
@@ -35,13 +32,10 @@ public function index() {
         'total_expenses' => $todayMoneyData['total_expenses'],
         'total_customers_today' => $todayCustomers,
         'customer_percentage_change' => $customerPercentageChange, // Pass percentage change to the view
-<<<<<<< HEAD
-=======
         'totalSuppliers' => $totalSuppliers,
         'percentageChange' => $percentageChange,
         'totalPurchaseorders' => $totalPurchaseorders
 
->>>>>>> main
     ]);
 }
 }
